3521c14e73c5a392861ed9ecb4dd9924a74ea143
Andrzzej <s28492@pjwstk.edu.pl>
2024-03-12 17:00:19 +0100

    Get Average Method

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-List<int> numbers = new List<int>(){1,2,3,4,5,6,7};
-Console.WriteLine(numbers.Average());
+static double getAverage(int[] numbers)
+{
+    double sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+    
+    //List<int> numbers = new List<int>(){1,2,3,4,5,6,7};
+    
+    return numbers.Average(); //hehe
+}
