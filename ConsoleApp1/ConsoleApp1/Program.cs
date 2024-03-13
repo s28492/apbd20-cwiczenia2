f94f0cdb66bb1062bc39e7077e4fae054f3a60db
Andrzzej <s28492@pjwstk.edu.pl>
2024-03-13 16:29:13 +0100

    find max value

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -3,14 +3,8 @@
 static double GetAverage(int[] numbers)
 {
     double sum = 0;
-    for (int i = 0; i < numbers.Length; i++)
-    {
-        sum += numbers[i];
-    }
     
-    //List<int> numbers = new List<int>(){1,2,3,4,5,6,7};
-    
-    return numbers.Average(); //hehe
+    return numbers.Max(); //hehe
 }
 int[] numbers = {1, 2, 3, 4, 5, 6};
 Console.WriteLine(GetAverage(numbers));
